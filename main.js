e276e41c62e4bec1a413dd64f64c141f3ff5594c
anoff <offenhaeuser@gmail.com>
2017-05-11 15:28:41 +0200

    :star2: feat: ONLY EMOJIS

--- main.js
+++ main.js
@@ -31,22 +31,6 @@ function fitScreen() {
   return [canvas.width, canvas.height]
 }
 
-// draw line to canvas
-//  points should be arrays of [x, y]
-function drawLine(ctx, points) {
-  const WIDTH = ctx.width;
-  ctx.beginPath();
-  ctx.strokeStyle = 'rgb(69, 69, 69)';
-  ctx.lineWidth = 2;
-  ctx.moveTo(0, points[0][1]);
-  for (let i = 1; i < points.length; i += 2) {
-    const bezP = points[i - 1];
-    const endP = points[i];
-    ctx.quadraticCurveTo(bezP[0], bezP[1], endP[0], endP[1]);
-  }
-  ctx.stroke();
-}
-
 // filter points array
 //  moving average of _width_
 function initFilter(pointsCount, width) {
@@ -82,19 +66,6 @@ function initFilter(pointsCount, width) {
   }
 }
 
-// generate a rainbow color in a range of 0..1
-function generateColor(pct) {
-  if (pct > 1) {
-    pct = 1;
-  } else if (pct < 0) {
-    pct = 0;
-  }
-  const r = Math.sin(pct * 4 * Math.PI + 0) * 127 + 128;
-  const g = Math.sin(pct * 4 * Math.PI + 2 * Math.PI / 3) * 127 + 128;
-  const b = Math.sin(pct * 4 * Math.PI + 4 * Math.PI / 3) * 127 + 128;
-  return 'rgb(' + Math.round(r) + ',' + Math.round(g) + ',' + Math.round(b) + ')';
-}
-
 // random emojis
 //  return an array of emojis
 function getEmoji(count = 1) {
@@ -121,6 +92,7 @@ function visualize() {
     const [WIDTH, HEIGHT] = fitScreen();
     analyser.getByteFrequencyData(dataArray);
 
+    // clear canvas
     canvasCtx.fillStyle = 'rgb(255, 255, 255)';
     canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
@@ -129,9 +101,6 @@ function visualize() {
     for(let i = 0; i < bufferLength; i++) {
       const barX = i * (barWidth + 1);
       const barHeight = dataArray[i] / 255 * HEIGHT;
-
-      canvasCtx.fillStyle = generateColor(i / bufferLength);
-      canvasCtx.fillRect(barX, HEIGHT, barWidth, -barHeight);
       points.push([barX + barWidth / 2, HEIGHT - barHeight]);
     }
     let filtered = points
@@ -140,17 +109,16 @@ function visualize() {
     filtered = filtered.map((e, i) => {
         return [points[i][0], e];
       });
-    // drawLine(canvasCtx, filtered); // uncomment this one to draw a bezier line of median
+    
     for (let i = 0; i < filtered.length; i++) {
       const p = filtered[i];
       // put dancing emoji
-      if (p[1] < HEIGHT) { // only show if there is a bar
+      for (let height = p[1] + barWidth / 2; height < HEIGHT * 1.1 /* give a little extra for bottom emojis*/; height += barWidth) {
         canvasCtx.font = `${barWidth}px Arial`;
-        canvasCtx.fillText(emojis[i], p[0] - barWidth / 2, p[1] + barWidth / 2);
+        canvasCtx.fillText(emojis[i], p[0] - barWidth / 2, height);
       }
     }
   }
 
   setInterval(() => requestAnimationFrame(draw), 10);
-
 }
