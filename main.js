74efcb84324067e6becaf3cac968b34325ba13e5
anoff <offenhaeuser@gmail.com>
2017-05-11 12:37:15 +0200

    :star2: feat: show moving average

--- main.js
+++ main.js
@@ -45,27 +45,64 @@ function drawLine(ctx, points) {
   ctx.stroke();
 }
 
+// filter points array
+//  moving average of _width_
+function initFilter(pointsCount, width) {
+  const values = new Array(pointsCount);
+  for (let i = 0; i < pointsCount; i++) {
+    values[i] = new Array(width);
+  }
+
+  function mean(vals) {
+    let cnt = 0;
+    let sum = 0;
+    for (const v of vals) {
+      if (typeof v === 'number') {
+        sum += v;
+        cnt++;
+      }
+    }
+    if (cnt === 0) {
+      return 0;
+    }
+    return sum / cnt;
+  }
+  return points => {
+    const means = [];
+    // shift stuff around in the cached array
+    for (let i = 0; i < values.length; i++) {
+      const v = values[i];
+      v.shift();
+      v.push(points[i]);
+      means.push(mean(v));
+    }
+    return means;
+  }
+}
+
+// generate a rainbow color in a range of 0..1
+function generateColor(pct) {
+  if (pct > 1) {
+    pct = 1;
+  } else if (pct < 0) {
+    pct = 0;
+  }
+  const r = Math.sin(pct * 4 * Math.PI + 0) * 127 + 128;
+  const g = Math.sin(pct * 4 * Math.PI + 2 * Math.PI / 3) * 127 + 128;
+  const b = Math.sin(pct * 4 * Math.PI + 4 * Math.PI / 3) * 127 + 128;
+  return 'rgb(' + Math.round(r) + ',' + Math.round(g) + ',' + Math.round(b) + ')';
+}
+
 // start visualizing
 function visualize() {
 
-  analyser.fftSize = 512;
+  analyser.fftSize = 256;
   const bufferLength = analyser.frequencyBinCount;
   const dataArray = new Uint8Array(bufferLength);
 
+  const filterPoints = initFilter(bufferLength, 50);
   function draw() {
     const [WIDTH, HEIGHT] = fitScreen();
-    // generate a rainbow color in a range of 0..1
-    function generateColor(pct) {
-      if (pct > 1) {
-        pct = 1;
-      } else if (pct < 0) {
-        pct = 0;
-      }
-      const r = Math.sin(pct * 4 * Math.PI + 0) * 127 + 128;
-      const g = Math.sin(pct * 4 * Math.PI + 2 * Math.PI / 3) * 127 + 128;
-      const b = Math.sin(pct * 4 * Math.PI + 4 * Math.PI / 3) * 127 + 128;
-      return 'rgb(' + Math.round(r) + ',' + Math.round(g) + ',' + Math.round(b) + ')';
-    }
     analyser.getByteFrequencyData(dataArray);
 
     canvasCtx.fillStyle = 'rgb(255, 255, 255)';
@@ -81,8 +118,15 @@ function visualize() {
       canvasCtx.fillRect(barX, HEIGHT, barWidth, -barHeight);
       points.push([barX + barWidth / 2, HEIGHT - barHeight]);
     }
-    drawLine(canvasCtx, points);
+    let filtered = points
+      .map(e => e[1]);
+    filtered = filterPoints(filtered);
+    filtered = filtered.map((e, i) => {
+        return [points[i][0], e];
+      });
+    drawLine(canvasCtx, filtered);
   }
 
   setInterval(() => requestAnimationFrame(draw), 10);
+
 }
