5fd8eabf870cea4475cc5bbfeef90b7faed4c612
anoff <offenhaeuser@gmail.com>
2017-06-25 21:10:26 +0200

    :star2: feat: hide canvas while drawing

--- main.js
+++ main.js
@@ -73,6 +73,7 @@ function visualize() {
   const emojis = getEmoji(bufferLength);
 
   function draw() {
+    canvas.style = "display: none";
     stats.begin();
     const [WIDTH, HEIGHT] = fitScreen();
     analyser.getByteFrequencyData(dataArray);
@@ -105,6 +106,41 @@ function visualize() {
     }
     requestAnimationFrame(draw);
     stats.end();
+    canvas.style = "display: block";
   }
   draw();
 }
+
+/*
+var app = new PIXI.Application(800, 600, {backgroundColor: 0x1099bb});
+document.body.appendChild(app.view);
+
+var basicText = new PIXI.Text('Basic text in pixi');
+basicText.x = 30;
+basicText.y = 90;
+
+app.stage.addChild(basicText);
+
+var style = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 36,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    fill: ['#ffffff', '#00ff99'], // gradient
+    stroke: '#4a1850',
+    strokeThickness: 5,
+    dropShadow: true,
+    dropShadowColor: '#000000',
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+    wordWrap: true,
+    wordWrapWidth: 440
+});
+
+var richText = new PIXI.Text('Rich text with a lot of options and across multiple lines', style);
+richText.x = 30;
+richText.y = 180;
+
+app.stage.addChild(richText);
+*/
